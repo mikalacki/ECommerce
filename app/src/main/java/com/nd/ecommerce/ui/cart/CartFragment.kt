21a7283d1769564e986de351f3c617b46f8e4ffb
milosmikalacki <milos@prodactivesolutions.com>
2026-03-19 22:47:23 +0100

    - initial UI implemented

---
+++ app/src/main/java/com/nd/ecommerce/ui/cart/CartFragment.kt
@@ -0,0 +1,28 @@
+package com.nd.ecommerce.ui.cart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nd.ecommerce.databinding.FragmentCartBinding
+
+class CartFragment : Fragment() {
+
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
